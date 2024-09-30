1287e6d73cdec3fe44769049005ec4487ddf3652
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2024-09-30 14:31:55 -0300

    Add animation to inputs Footer

--- components/footerComp.js
+++ components/footerComp.js
@@ -4,23 +4,19 @@ export const footerComp =
             <h2>Contáctanos</h2>
             <div class="footerForm">
                 <form class="Form">
-                    <input type="text" placeholder="Nombre">
-                    <input type="email"placeholder="Email">
+                    <input class="FormInput" type="text" placeholder="Nombre">
+                    <input class="FormInput" type="email"placeholder="Email">
                     <button class="btnSubmit" type="submit">Enviar</button>
                 </form>
                 <h3 class="break-ó">ó</h3>
                 <a href="mailto:maxiluma18@gmail.com"><button class="btnFooter">
                     <svg viewBox="0 0 24 24" class="arr-2" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                        d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-                        ></path>
+                        <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                     </svg>
                     <span class="text">Nuestro Mail</span>
                     <span class="circle"></span>
                     <svg viewBox="0 0 24 24" class="arr-1" xmlns="http://www.w3.org/2000/svg">
-                        <path
-                        d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
-                        ></path>
+                        <path d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"></path>
                     </svg>
                     </button></a>
             </div>
