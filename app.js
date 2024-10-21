0b3fd73eeada519672fc0dc55b2388379e636352
Maximiliano Martinez <maxilucasmartinez18@gmail.com>
2024-10-21 13:14:42 -0300

    Fixed Routes and changed img

--- app.js
+++ app.js
@@ -4,24 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde la carpeta "public"
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta para el index.html
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Ruta para otra página
-app.get('/nosotros', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'nosotros.html'));
-  });
 
-  // Ruta para otra página
-app.get('/perfil', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'perfil.html'));
-  });
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
