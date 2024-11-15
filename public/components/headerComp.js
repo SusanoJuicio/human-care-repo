6e6b030c14d94a5953469975619432234b2fa1c8
Talia Ivonne Ojeda <ivonnetalia17@gmail.com>
2024-11-14 23:30:15 -0300

    intercabio de barra de busqueda y cambios en el boton

--- public/components/headerComp.js
+++ public/components/headerComp.js
@@ -6,27 +6,7 @@ export const headerComp = `
                     <a href="index.html" class="nav_brand">
                         <img class="nav_logo" src="/images/Logo.webp" alt="Logo de la empresa">
                     </a>
-                    
-                    <!-- Botón hamburguesa para móvil -->
-                    <button class="nav_toggle" aria-label="Abrir menú">
-                        <span class="hamburger"></span>
-                    </button>
-
-                    <div class="nav_search">
-                        <input 
-                            type="text" 
-                            class="nav_search_input" 
-                            placeholder="Buscar Producto"
-                            aria-label="Buscar producto"
-                        >
-                        <button class="nav_search_button" aria-label="Realizar búsqueda">
-                            <svg class="nav_search_icon" aria-hidden="true" viewBox="0 0 24 24">
-                                <g>
-                                    <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
-                                </g>
-                            </svg>
-                        </button>
-                    </div>
+                    <h1 class="titulo">HumanCare</h1>
 
                     <ul class="nav_list_lis">
                         <li class="nav_list_li">
